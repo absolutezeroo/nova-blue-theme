af1aaea0d910ba292527e93d047264b2c2948d77
absoluzeroo <ctuto37@gmail.com>
2021-02-09 15:42:28 +0100

    Initial commit

---
+++ src/ThemeServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Absolutezeroo\HabboTheme;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Nova;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::booted(function () {
+            Nova::theme(asset('/absolutezeroo/habbo-theme/theme.css'));
+        });
+
+        $this->publishes([
+            __DIR__.'/../resources/css' => public_path('absolutezeroo/habbo-theme'),
+        ], 'public');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
